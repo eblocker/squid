0d3801bf37230acd2e0b7a2fa09a66ea9b39172a
Amos Jeffries <amosjeffries@squid-cache.org>
2015-10-01 22:05:18 -0700

    Imported Upstream version 3.5.10

--- src/store.cc
+++ src/store.cc
@@ -144,25 +144,6 @@ Store::unlink (StoreEntry &anEntry)
     fatal("Store::unlink on invalid Store\n");
 }
 
-void *
-StoreEntry::operator new (size_t bytecount)
-{
-    assert (bytecount == sizeof (StoreEntry));
-
-    if (!pool) {
-        pool = memPoolCreate ("StoreEntry", bytecount);
-        pool->setChunkSize(2048 * 1024);
-    }
-
-    return pool->alloc();
-}
-
-void
-StoreEntry::operator delete (void *address)
-{
-    pool->freeOne(address);
-}
-
 void
 StoreEntry::makePublic()
 {

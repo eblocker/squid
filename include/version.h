bcc454e1e51c08523eb5c2d521bbd9d02ea36154
Amos Jeffries <amosjeffries@squid-cache.org>
2019-02-19 18:29:20 +1300

    New upstream version 4.6

--- include/version.h
+++ include/version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1996-2018 The Squid Software Foundation and contributors
+ * Copyright (C) 1996-2019 The Squid Software Foundation and contributors
  *
  * Squid software is distributed under GPLv2+ license and includes
  * contributions from numerous individuals and organizations.
@@ -7,7 +7,7 @@
  */
 
 #ifndef SQUID_RELEASE_TIME
-#define SQUID_RELEASE_TIME 1546302637
+#define SQUID_RELEASE_TIME 1550545471
 #endif
 
 /*
